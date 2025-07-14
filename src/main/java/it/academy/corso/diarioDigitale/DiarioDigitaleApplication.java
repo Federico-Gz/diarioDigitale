02ab2d61c9caa069bb07dcde48bab3d28f60cde8
Federico-Gz <federicograziani96@gmail.com>
2025-07-14 12:04:38 +0200

    Initial commit

---
+++ src/main/java/it/academy/corso/diarioDigitale/DiarioDigitaleApplication.java
@@ -0,0 +1,13 @@
+package it.academy.corso.diarioDigitale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDigitaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDigitaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/academy/corso/diarioDigitale/DiarioDigitaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy.corso.diarioDigitale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDigitaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
